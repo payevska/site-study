26817120c132a74fdcdee934851c8b9eb3362286
Yuliya Payevska <y.payevska@gmail.com>
2020-01-15 22:27:49 +0200

    did section services

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 $(function(){
 
-  $('.slider__inner, .news__slider-inner').slick({
-    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    infinite: false
-  });
-
-  $('select').styler();
-
-  $('.header__btn-menu').on('click', function(){
-    $('.menu ul').slideToggle();
-  });
+	//slider
+  $('.slider__inner').slick({ // $('.single-item').slick();  https://kenwheeler.github.io/slick/
+  	nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+  	prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+  	infinite: false
+  });	
 
 });
