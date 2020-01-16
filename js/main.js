4b860dc4a492440f799d073ad0c892ed4d1e6eb4
Yuliya Payevska <y.payevska@gmail.com>
2020-01-16 22:08:25 +0200

    did slider in section news

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(function(){
 
 	//slider
-  $('.slider__inner').slick({ // $('.single-item').slick();  https://kenwheeler.github.io/slick/
+  $('.slider__inner, .news__slider-inner').slick({ // $('.single-item').slick();  https://kenwheeler.github.io/slick/
   	nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
   	prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
   	infinite: false
-  });	
+  });
+
+  $('select').styler();
 
 });
